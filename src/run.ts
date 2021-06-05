572b2012168cfc18fe9da994813cdec1ce5b4b00
Hidetake Iwata <int128@gmail.com>
2021-06-05 16:50:48 +0900

    Refactor: extract push()

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { promises as fs } from 'fs'
-import { queryBaseGitObject } from './base-git-object'
+import { push } from './git'
 import { globTreeFiles } from './glob'
 
 interface Inputs {
@@ -16,12 +16,6 @@ interface Inputs {
 export const run = async (inputs: Inputs): Promise<void> => {
   const [owner, repo] = inputs.repository.split('/')
   const octokit = github.getOctokit(inputs.token)
-
-  const baseGitObject = await queryBaseGitObject(octokit, { owner, repo, ref: inputs.ref })
-  core.info(`found base ${JSON.stringify(baseGitObject, undefined, 2)}`)
-  if (baseGitObject?.repository?.ref?.target?.__typename !== 'Commit') {
-    throw new Error(`unexpected query response: typename == ${baseGitObject?.repository?.ref?.target?.__typename}`)
-  }
 
   const treeFiles = await globTreeFiles(inputs.baseDirectory, inputs.path)
   const treeEntries = await Promise.all(
@@ -42,45 +36,11 @@ export const run = async (inputs: Inputs): Promise<void> => {
     })
   )
 
-  const { data: tree } = await octokit.rest.git.createTree({
+  await push(octokit, {
     owner,
     repo,
-    tree: treeEntries,
-    base_tree: baseGitObject.repository.ref.target.tree.oid,
-  })
-  core.info(`created tree ${tree.sha}`)
-
-  const { data: commit } = await octokit.rest.git.createCommit({
-    owner,
-    repo,
-    tree: tree.sha,
-    parents: [baseGitObject.repository.ref.target.oid],
+    ref: inputs.ref,
     message: inputs.message,
+    tree: treeEntries,
   })
-  core.info(`created commit ${commit.sha}`)
-
-  const { data: commitDetail } = await octokit.rest.repos.getCommit({
-    owner,
-    repo,
-    ref: commit.sha,
-  })
-  if (commitDetail.files === undefined) {
-    throw new Error(`unexpected error: octokit.repos.getCommit().files === undefined`)
-  }
-  if (commitDetail.files.length === 0) {
-    core.info(`nothing to commit`)
-    return
-  }
-  for (const f of commitDetail.files) {
-    core.info(`commit: ${f.status} ${f.filename} (+${f.additions} -${f.deletions})`)
-  }
-
-  const { data: updatedRef } = await octokit.rest.git.updateRef({
-    owner,
-    repo,
-    ref: `heads/${baseGitObject.repository.ref.name}`,
-    sha: commit.sha,
-  })
-  core.info(`updated ref ${updatedRef.ref} to ${updatedRef.object.sha}`)
-  return
 }
