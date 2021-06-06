c2b31abd46f7379f5e29b7e5b372382803c7b6dd
Hidetake Iwata <int128@gmail.com>
2021-06-06 12:16:32 +0900

    Create branch if not exists (#51)

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # create-commit-action
-GitHub Action to create a commit using GitHub API
+
+This is an cction to push files using GitHub API.
+
+
+## Getting Started
+
+To create a commit into a branch:
+
+```yaml
+jobs:
+  build:
+    steps:
+      - uses: int128/create-commit-action@v1
+        with:
+          repository: ${{ github.repository }}
+          ref: refs/heads/branch-name
+          path: glob-pattern
+          message: your-commit-message
+```
+
+If a branch does not exist, this action creates it from default branch.
+Otherwise, this action updates it.
--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -13,7 +13,16 @@ export type BaseGitObjectQuery = (
     { __typename?: 'Repository' }
     & { ref?: Types.Maybe<(
       { __typename?: 'Ref' }
-      & Pick<Types.Ref, 'prefix' | 'name'>
+      & { target?: Types.Maybe<{ __typename: 'Blob' } | (
+        { __typename: 'Commit' }
+        & Pick<Types.Commit, 'oid'>
+        & { tree: (
+          { __typename?: 'Tree' }
+          & Pick<Types.Tree, 'oid'>
+        ) }
+      ) | { __typename: 'Tag' } | { __typename: 'Tree' }> }
+    )>, defaultBranchRef?: Types.Maybe<(
+      { __typename?: 'Ref' }
       & { target?: Types.Maybe<{ __typename: 'Blob' } | (
         { __typename: 'Commit' }
         & Pick<Types.Commit, 'oid'>
