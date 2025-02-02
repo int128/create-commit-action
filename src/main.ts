64604ef5489a283f97583ed0d377037d0a0c6239
Hidetake Iwata <int128@gmail.com>
2025-02-02 19:56:15 +0900

    Refactor (#1201)

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import * as core from '@actions/core'
+import { Octokit } from '@octokit/action'
 import { run } from './run.js'
 
 const main = async (): Promise<void> => {
-  await run({
-    repository: core.getInput('repository', { required: true }),
-    ref: core.getInput('ref', { required: true }),
-    path: core.getInput('path', { required: true }),
-    baseDirectory: core.getInput('base-directory', { required: true }),
-    message: core.getInput('message', { required: true }),
-    token: core.getInput('token', { required: true }),
-  })
+  await run(
+    {
+      repository: core.getInput('repository', { required: true }),
+      ref: core.getInput('ref', { required: true }),
+      path: core.getInput('path', { required: true }),
+      baseDirectory: core.getInput('base-directory', { required: true }),
+      message: core.getInput('message', { required: true }),
+    },
+    new Octokit(),
+  )
 }
 
 main().catch((e: Error) => {
